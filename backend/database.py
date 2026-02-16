ad1aafa8836e7101e15dae413bf50a909dd9bfac
Praveen <praveen994490@gmail.com>
2026-02-16 11:00:11 +0530

    fixed database url

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,27 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# DATABASE URL (address of database)
-DATABASE_URL = "postgresql://postgres:praveen%4027@localhost:5432/pgfinder"
+# DATABASE URL from Render Environment Variable
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# CREATE ENGINE → Connect to PostgreSQL
-engine = create_engine(DATABASE_URL)
+# CREATE ENGINE → Connect to Render PostgreSQL
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
-#  CREATE SESSION → for DB operations
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# CREATE SESSION → for DB operations
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False
+)
 
-#  BASE CLASS → for all models
+# BASE CLASS → for all models
 Base = declarative_base()
 
-#  DEPENDENCY → used in FastAPI routes to get DB session
+# DEPENDENCY → used in FastAPI routes to get DB session
 def get_db():
     db = SessionLocal()
     try:
